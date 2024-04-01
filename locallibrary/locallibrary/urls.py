dd433990ae23285db45207edd2db4d5dfcfcc1fe
osamu <osamu.shibuya@ndsu.edu>
2024-04-01 14:07:48 -0500

    until tutorial6

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -23,8 +23,15 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+]
+
+
+urlpatterns += [
     path('catalog/', include('catalog.urls')),
-    path('', RedirectView.as_view(url='catalog/')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
+urlpatterns += [
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
+]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
